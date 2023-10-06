f06e50f25a69a06e9daf13269731d40987e0f78b
Florheim <fwizard0719@gmail.com>
2023-10-06 19:53:12 +0800

    feat: added javascript code into my html

---
+++ Activity2.js
@@ -0,0 +1,14 @@
+var firstFavNumtFavNum = 100;
+var secondFavNum = 65;
+
+console.log(firstFavNumtFavNum > secondFavNum);
+console.log(firstFavNumtFavNum < secondFavNum);
+console.log(firstFavNumtFavNum >= secondFavNum);
+console.log(firstFavNumtFavNum <= secondFavNum);
+console.log(firstFavNumtFavNum === secondFavNum);
+console.log(firstFavNumtFavNum == secondFavNum);
+console.log(firstFavNumtFavNum !== secondFavNum);
+console.log(firstFavNumtFavNum != secondFavNum);
+
+
+
